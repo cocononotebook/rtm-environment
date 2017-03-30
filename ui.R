1f56d4c90d0706999fd3e007134a37bc68844936
cocononotebook <cocononotebook@gmail.com>
2017-03-30 13:53:26 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,18 +8,30 @@ shinyUI(
     #location:"static-top","fixed-top","fixed-bottom"
     tabPanel("The First Time Investigation"),
     tabPanel("The Second Time Investigation"),
-    
-    mainPanel(
-      headerPanel("GodDamnTitle"),
-      checkboxGroupInput("CBGI","checkboxGroupInput",c("A","B","c","D","E","F")
-      #checkboxGroupInput(id="",label="",GroupBoxName="c("","")")
-      ,
-      tabsetPanel(
-        tabPanel("Summary"),
-        tabPanel("Cloud"),
-        tabPanel("Frequency")
-      )
+ 
+    sidebarLayout(
+        fluid=FALSE,
+        sidebarPanel(  
+          checkboxGroupInput("CBGI","checkboxGroupInput",c("A","B","c","D","E","F")
+                         #checkboxGroupInput(id="",label="",GroupBoxName="c("","")")
+          ),
+        navlistPanel(id="NLP",
+          "Student_List",
+          well=FALSE,
+          tabPanel("Student1"),
+          tabPanel("Student2"),
+          tabPanel("Student3")
+          )
+        ),
+          
+        mainPanel(
+          tabsetPanel(
+            tabPanel("Summary"),
+            tabPanel("Cloud"),
+            tabPanel("Frequency")
+          )
+        )
+      
     )
   )
-)
 )
